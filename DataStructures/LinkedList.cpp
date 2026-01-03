476279eec0cefb804a2a6eb35c230a0e572d6f01
ri <convolutionarynodes@proton.me>
2026-01-03 18:17:05 +0000

    added methods to break the circular loop and detect the actual loop via two pointer solution; also implemented a weak pointer for the last node in the linked list so that it can break the circular references to prevent memory leaks

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -3,9 +3,20 @@
 
 Node::Node(int val) : value(val), nextPointer(nullptr) {}
 
-LinkedList::LinkedList() : head(nullptr) {}
+LinkedList::LinkedList() : head(nullptr), isCircularList(false) {}
+
+LinkedList::~LinkedList() {
+    if (isCircularList) {
+        breakCircular();
+    }
+}
 
 void LinkedList::insertNode(std::shared_ptr<Node> newNode) {
+    if (isCircularList) {
+        std::cout << "Cannot insert into circular linked list \n";
+        return;
+    }
+
     if (head == nullptr) {
         head = newNode;
         return;
@@ -19,6 +30,11 @@ void LinkedList::insertNode(std::shared_ptr<Node> newNode) {
 }
 
 void LinkedList::deleteNode(int value) {
+    if (isCircularList) {
+        std::cout << "Cannot delete from a circular list \n";
+        return;
+    }
+
     if (!head) {
         std::cout << "List is empty\n";
         return;
@@ -55,7 +71,7 @@ void LinkedList::findNode(int value) {
     }
     
     std::shared_ptr<Node> currentNode = head->nextPointer;
-    while (currentNode) {
+    while (currentNode && currentNode != head) {
         if (currentNode->value == value) {
             std::cout << "Node found in linked list\n";
             return;
@@ -67,12 +83,58 @@ void LinkedList::findNode(int value) {
 }
 
 void LinkedList::makeCircular() {
-    if (!head) return;
+    if (!head) {
+        std::cout << "Cannot make empty list circular \n";
+        return;
+    }
+
+    if (isCircularList) {
+        std::cout << "List is already circularly linked \n";
+        return;
+    }
+
     
     std::shared_ptr<Node> currentNode = head;
     while (currentNode->nextPointer) {
         currentNode = currentNode->nextPointer;
     }
     
+    endNode = currentNode;
     currentNode->nextPointer = head;
+    isCircularList = true;
+
+    std::cout << "Linked list is now circular \n";
+}
+
+void LinkedList::breakCircular() {
+    if (!isCircularList) {
+        std::cout << "List isn't circular \n";
+        return;
+    }
+
+    if (auto lastNode = endNode.lock()) {
+        lastNode->nextPointer = nullptr;
+        std::cout << "List is no longer circular";
+    }
+
+    isCircularList = false;
+    endNode.reset();
+}
+
+bool LinkedList::isCircular() {
+    if (!head) return false;
+
+    std::shared_ptr<Node> slowPointer = head;
+    std::shared_ptr<Node> fastPointer = head;
+
+    while (fastPointer && fastPointer) {
+        slowPointer = slowPointer->nextPointer;
+        fastPointer = fastPointer->nextPointer->nextPointer;
+
+        if (slowPointer == fastPointer) {
+            return true;
+        }
+    }
+    
+    return false;
 }
